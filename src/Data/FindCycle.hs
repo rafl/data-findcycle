14e432f581c240387f4c8582f10856c5374e1220
Florian Ragwitz <florian.ragwitz@gmail.com>
2025-02-14 21:53:33 -0800

    Initial implementation for Floyd

--- src/Data/FindCycle.hs
+++ src/Data/FindCycle.hs
@@ -1,4 +1,34 @@
-module MyLib (someFunc) where
+module Data.FindCycle
+  ( findCycleLenFloyd
+  , findCycleFloyd
+  , findCycleLenFloydL
+  , findCycleFloydL) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+findCycleLenFloyd :: (Eq a) => (a -> a) -> a -> (Int, Int)
+findCycleLenFloyd f a = findCycleLenFloydL (iterate f a)
+
+findCycleFloyd :: (Eq a) => (a -> a) -> a -> ([a], [a])
+findCycleFloyd f a = findCycleFloydL (iterate f a)
+
+findCycleLenFloydL :: (Eq a) => [a] -> (Int, Int)
+findCycleLenFloydL xs = detectCycle 0 xs xs
+  where detectCycle n (t:ts) (_:h:hs)
+          | t == h = findMu 0 xs ts
+          | otherwise = detectCycle (n+1) ts hs
+        detectCycle n _ [_] = (2*n+1, 0)
+        detectCycle n _ _ = (2*n, 0)
+
+        findMu mu (t:ts) (m:ms)
+          | t == m = (mu, 1+findLambda m ms)
+          | otherwise = findMu (mu+1) ts ms
+        findMu mu _ _ = (mu, 0)
+
+        findLambda m (x:xs')
+          | m == x = 0
+          | otherwise = 1+findLambda m xs'
+        findLambda _ [] = 0
+
+findCycleFloydL :: (Eq a) => [a] -> ([a], [a])
+findCycleFloydL xs = (pre, take lambda cyc)
+  where (mu, lambda) = findCycleLenFloydL xs
+        (pre, cyc) = splitAt mu xs
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,37 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main (main) where
 
+import Test.QuickCheck
+import System.Exit
+import Control.Monad
+import Data.FindCycle
+
+prop_FloydLenL_nocycle :: NonNegative Int -> Property
+prop_FloydLenL_nocycle (NonNegative n) = findCycleLenFloydL [1..n] === (n, 0)
+
+prop_FloydLenL_cycle :: NonNegative Int -> Positive Int -> Property
+prop_FloydLenL_cycle (NonNegative a) (Positive b) =
+  findCycleLenFloydL ([1..a] ++ cycle [a+1 .. a+b]) === (a, b)
+
+prop_FloydL_nocycle :: NonNegative Int -> Property
+prop_FloydL_nocycle (NonNegative n) = findCycleFloydL xs === (xs, [])
+  where xs = [1..n]
+
+prop_FloydL_cycle :: NonNegative Int -> Positive Int -> Property
+prop_FloydL_cycle (NonNegative a) (Positive b) =
+  findCycleFloydL (as ++ cycle bs) === (as, bs)
+  where as = [1..a]
+        bs = [a+1..a+b]
+
+prop_FloydLen_id :: Int -> Property
+prop_FloydLen_id n = findCycleLenFloyd id n === (0, 1)
+
+prop_FloydLen_mod :: Int -> Positive Int -> Property
+prop_FloydLen_mod a (Positive b) =
+  findCycleLenFloyd (\x -> (x+1) `mod` b) (a `mod` b) === (0, b)
+
+$(return [])
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = $(quickCheckAll) >>= (`unless` exitFailure)
