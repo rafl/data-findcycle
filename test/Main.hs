3bc517ee61998f7a91dba1f3963b27b5b45a5821
Florian Ragwitz <florian.ragwitz@gmail.com>
2025-02-25 18:27:29 -0800

    Update tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,37 +1,162 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards, ScopedTypeVariables #-}
 
-module Main (main) where
-
-import Test.QuickCheck
-import System.Exit
-import Control.Monad
 import Data.FindCycle
+import Data.Numbers.Primes
+import Data.List
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NE
+import Data.Foldable
 
-prop_FloydLenL_nocycle :: NonNegative Int -> Property
-prop_FloydLenL_nocycle (NonNegative n) = findCycleLenFloydL [1..n] === (n, 0)
+data Cycle = Cycle {
+  cycMu, cycLambda :: Int,
+  cycF :: Integer -> Integer,
+  cycX0 :: Integer
+}
 
-prop_FloydLenL_cycle :: NonNegative Int -> Positive Int -> Property
-prop_FloydLenL_cycle (NonNegative a) (Positive b) =
-  findCycleLenFloydL ([1..a] ++ cycle [a+1 .. a+b]) === (a, b)
+instance Show Cycle where
+  show Cycle{..} = unwords [ "Cycle", show cycMu, show cycLambda ]
 
-prop_FloydL_nocycle :: NonNegative Int -> Property
-prop_FloydL_nocycle (NonNegative n) = findCycleFloydL xs === (xs, [])
-  where xs = [1..n]
+instance Arbitrary Cycle where
+  arbitrary = do
+    (Positive muPlusLambda) <- scale smoothScale arbitrary
+    mu <- chooseInt (0, muPlusLambda-1)
+    let Just m = find (> fromIntegral muPlusLambda) primes
+    -- TODO: just pick using Large suchThat mod m /= 0?
+    let nonZeroModM = choose (1, m-1)
+    (f, x0) <- mkF mu (muPlusLambda-mu) m <$> nonZeroModM <*> nonZeroModM
+    return (Cycle mu (muPlusLambda-mu) f x0)
+    where
+      smoothScale s = max 1 (round $ (10**5 :: Double) ** (fromIntegral s / 120.0))
+      mkF mu lambda m a b = (f, g 0)
+        where n = fromIntegral $ mu+lambda
+              g i = (a*i+b) `mod` m
+              f x | i < n-1 = g (i+1)
+                  | otherwise = g (fromIntegral mu)
+                where i = (modInv a m * ((x-b) `mod` m)) `mod` m
+      modInv 1 _ = 1
+      modInv x y = (a*y+1) `div` x
+        where a = x - modInv (y `mod` x) x
 
-prop_FloydL_cycle :: NonNegative Int -> Positive Int -> Property
-prop_FloydL_cycle (NonNegative a) (Positive b) =
-  findCycleFloydL (as ++ cycle bs) === (as, bs)
-  where as = [1..a]
-        bs = [a+1..a+b]
+totalAlgs :: NonEmpty (String, CycleFinder Integer)
+totalAlgs = NE.fromList
+  [ ("naiveHashable", naiveHashable)
+  , ("naiveOrd", naiveOrd)
+  ]
 
-prop_FloydLen_id :: Int -> Property
-prop_FloydLen_id n = findCycleLenFloyd id n === (0, 1)
+partialAlgs :: NonEmpty (String, CycleFinder Integer)
+partialAlgs = NE.fromList
+  [ ("nivash", nivash)
+  , ("brent", brent)
+  , ("floyd", floyd)
+  ]
 
-prop_FloydLen_mod :: Int -> Positive Int -> Property
-prop_FloydLen_mod a (Positive b) =
-  findCycleLenFloyd (\x -> (x+1) `mod` b) (a `mod` b) === (0, b)
+defAlg :: CycleFinder Integer
+defAlg = snd (NE.head partialAlgs)
 
-$(return [])
+type Runner r = Cycle -> r
+type Extractor = Runner (Int, Int, ([Integer], [Integer]))
+
+extractors :: NonEmpty (String, CycleFinder Integer -> Extractor)
+extractors = NE.fromList
+  [ ("findCycleExtract",
+      \alg Cycle{..} -> findCycleExtract alg cycF cycX0)
+  , ("unsafeFindCycleFromList",
+      \alg Cycle{..} -> unsafeFindCycleFromList alg (iterate cycF cycX0))
+  ]
+
+defExtractor :: CycleFinder Integer -> Extractor
+defExtractor = snd (NE.head extractors)
+
+discardExtract :: Runner (a, a, b) -> Runner (a, a)
+discardExtract f c = (mu, lambda)
+  where (mu, lambda, _) = f c
+
+prop_algCorrect :: CycleFinder Integer -> Cycle -> Property
+prop_algCorrect alg Cycle{..} = findCycle alg cycF cycX0 === (cycMu, cycLambda)
+
+prop_algsAgree :: CycleFinder Integer -> CycleFinder Integer -> Cycle -> Property
+prop_algsAgree a b Cycle{..} =
+  findCycleExtract a cycF cycX0 === findCycleExtract b cycF cycX0
+
+prop_runnersAgree :: (Eq r, Show r) => Runner r -> Runner r -> Cycle -> Property
+prop_runnersAgree a b c = a c === b c
+
+prop_muUpperBound :: CycleFinder Integer -> Cycle -> Property
+prop_muUpperBound alg Cycle{..} =
+  counterexample ("Should be at least " ++ show cycMu) $
+    fst (findCycle alg cycF cycX0) >= cycMu
+
+prop_extract :: CycleFinder Integer -> Cycle -> Property
+prop_extract alg Cycle{..} =
+  (length pre, length cyc, pre ++ cyc ++ cyc)
+    === (mu, lambda, take (mu+2*lambda) (iterate cycF cycX0))
+  where (mu, lambda, (pre, cyc)) = findCycleExtract alg cycF cycX0
+
+prop_cycleExp :: CycleFinder Integer -> Cycle -> Property
+prop_cycleExp alg Cycle{..} =
+  property $ \(NonNegative n) ->
+    counterexample ("n=" ++ show n) $
+      g (fromIntegral n)
+        === xs !! (n - max 0 (n-cycMu - ((n-cycMu) `mod` cycLambda)))
+  where g = cycleExp alg cycF cycX0
+        xs = iterate cycF cycX0
+
+prop_cycleExpsAgree alg Cycle{..} =
+  property $ \(NonNegative (n :: Int)) ->
+    counterexample ("n=" ++ show n) $
+      g (fromIntegral n) === g' (fromIntegral n)
+  where g = cycleExp alg cycF cycX0
+        g' = cycleExp' alg cycF cycX0
+
+prop_finite :: CycleFinder Integer -> Cycle -> Property
+prop_finite alg Cycle{..} =
+  unsafeFindCycleFromList (minimalMu alg) (take cycMu xs)
+    === (cycMu, 0, (take cycMu xs, []))
+  where xs = iterate cycF cycX0
+
+tests :: TestTree
+tests = testGroup "Data.FindCycle minimal tests"
+  [
+    let (name, alg) :| _ = totalAlgs in
+    testProperty (name ++ " is correct")
+      (prop_algCorrect alg)
+  , testGroup "All total algorithms agree" [
+      testProperty (refName ++ " ~ " ++ name) (prop_algsAgree refAlg alg)
+      | let ((refName, refAlg) :| algs) = totalAlgs,
+        (name, alg) <- algs
+    ]
+  , testGroup "Total agrees with minimalMu of partial" [
+      testProperty (refName ++ " ~ minimalMu " ++ algName)
+        (prop_algsAgree refAlg (minimalMu alg))
+      | let ((refName, refAlg) :| _) = totalAlgs,
+        (algName, alg) <- toList partialAlgs
+    ]
+  , testGroup "mu upper bound for partial" [
+      testProperty name (prop_muUpperBound alg)
+      | (name, alg) <- toList partialAlgs
+    ]
+  , testProperty "minimalMu is idempotent"
+      (prop_algsAgree (minimalMu defAlg) (minimalMu (minimalMu defAlg)))
+  , testGroup "Extractors agree" [
+      testProperty (refName ++ " ~ " ++ rName)
+        (prop_runnersAgree (refR defAlg) (r defAlg))
+      | let ((refName, refR) :| rs) = extractors,
+        (rName, r) <- rs
+    ]
+  , testProperty "findCycle agrees with extractors"
+      (prop_runnersAgree
+         (discardExtract (defExtractor defAlg))
+         (\Cycle{..} -> findCycle defAlg cycF cycX0))
+  , testProperty "extraction is correct" (prop_extract defAlg)
+  , testProperty "cycleExp matches naive iteration" (prop_cycleExp defAlg)
+  , testProperty "cycleExp' agrees with cycleExp " (prop_cycleExpsAgree defAlg)
+  , testGroup "finite lists" [
+      testProperty name (prop_finite alg)
+      | (name, alg) <- toList (totalAlgs <> partialAlgs)
+    ]
+  ]
 
 main :: IO ()
-main = $(quickCheckAll) >>= (`unless` exitFailure)
+main = defaultMain tests
